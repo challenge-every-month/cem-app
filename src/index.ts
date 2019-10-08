e7f6e62ebecbb4a8684da106260202e727ccbee1
Aqui TSUCHIDA <aqui.tcd+dev@gmail.com>
2019-10-09 00:08:46 +0900

    feat(index): 削除コマンド系を登録

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,9 @@ import echo from './commands/echo'
 import cem_register from './commands/cem_register'
 import cem_new from './commands/cem_new'
 import cem_publish from './commands/cem_publish'
+import cem_delete from './commands/cem_delete'
 import res_cem_new from './listeners/res_cem_new'
+import res_cem_delete from './listeners/res_cem_delete'
 ;(async () => {
   // Start your app
   await app.start(process.env.PORT || 3000)
@@ -13,5 +15,7 @@ import res_cem_new from './listeners/res_cem_new'
 echo()
 cem_register()
 cem_new()
-res_cem_new()
 cem_publish()
+cem_delete()
+res_cem_new()
+res_cem_delete()
