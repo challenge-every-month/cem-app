a2666cd27c38a36ee3cd1e9160fd229632d8f9e9
Aqui TSUCHIDA <aqui.tcd+dev@gmail.com>
2019-10-11 00:54:30 +0900

    feat(index): レビューコマンドの実装

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,14 @@ import cem_register from './commands/cem_register'
 import cem_new from './commands/cem_new'
 import cem_publish from './commands/cem_publish'
 import cem_delete from './commands/cem_delete'
+import cem_review from './commands/cem_review'
 import res_cem_new from './listeners/res_cem_new'
 import res_cem_delete from './listeners/res_cem_delete'
+import res_cem_review_challenge from './listeners/res_cem_review_challenge'
+import res_cem_review_all from './listeners/res_cem_review_all'
 
 import remind from './requests/remind'
-import deploying from './requests/deploying'
-import deployed from './requests/deployed'
+import deploy from './requests/deploy'
 ;(async () => {
   // Start your app
   // await app.receiver.start()
@@ -22,9 +24,11 @@ echo()
 cem_register()
 cem_new()
 cem_publish()
+cem_review()
 cem_delete()
 res_cem_new()
 res_cem_delete()
+res_cem_review_challenge()
+res_cem_review_all()
 remind()
-deploying()
-deployed()
+deploy()
