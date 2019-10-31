bb8995346a459c53974b44a8b5f304cd030592e9
Aqui TSUCHIDA <aqui.tcd+dev@gmail.com>
2019-10-31 21:32:12 +0900

    fix(remind): Cronからのみ実行可能なように修正

--- src/requests/remind.ts
+++ src/requests/remind.ts
@@ -5,6 +5,7 @@ const config = require(`config`)
 export default function() {
   app.receiver.app.get(`/slack/remind`, (req, res) => {
     res.sendStatus(200)
+    const fromCron = req.header(`X-Appengine-Cron`) === `true`
     const channels = config.get(`Slack.Channels`)
     const now = new Date()
     const today = now.getDate()
@@ -12,6 +13,7 @@ export default function() {
 
     let text = ``
     let channel = ``
+    let postingMsg = true
     if (today === 1) {
       text = `新しい月の始まりです！今月も目標を立てて一歩一歩行きましょう！Let's Challenge！`
       channel = channels.publish
@@ -21,14 +23,27 @@ export default function() {
     } else if (tommorrow === 1) {
       text = `今月もお疲れさまでした。振り返りをしましょう！`
       channel = channels.review
+    } else {
+      postingMsg = false
     }
     const msg: Message = {
       token: process.env.SLACK_BOT_TOKEN,
       text: `<!channel>\n${text}`,
       channel: channel,
     }
-    return app.client.chat.postMessage(msg).catch(err => {
-      throw new Error(err)
-    })
+    if (fromCron && postingMsg) {
+      return app.client.chat.postMessage(msg).catch(err => {
+        throw new Error(err)
+      })
+    } else {
+      let msg = ``
+      if (!fromCron) {
+        msg += `the request does not come from cron\n`
+      }
+      if (!postingMsg) {
+        msg += `it is not time to remind\n`
+      }
+      console.log(msg)
+    }
   })
 }
