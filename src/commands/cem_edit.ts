4a03e14dfa09bcf4c3f2bf316ac05734400f41d9
nainaistar <nainaistar@gmail.com>
2020-03-29 17:35:17 +0900

    add: WIP:project数文読み込むように修正

--- src/commands/cem_edit.ts
+++ src/commands/cem_edit.ts
@@ -1,5 +1,5 @@
 import { app } from '../initializers/bolt'
-import { Option, Modal, Message } from '../types/slack'
+import { Option, Modal, Message, ModalBlock } from '../types/slack'
 import { firestore } from '../initializers/firebase'
 
 app.command(`/cem_edit`, async ({ payload, ack, context }) => {
@@ -38,6 +38,152 @@ app.command(`/cem_edit`, async ({ payload, ack, context }) => {
       value: `${m + 1}`,
     }
   })
+
+  // const blocks2: ModalBlock[] =
+  const blocks2: ModalBlock[] = []
+  projects.docs.forEach((project, index) => {
+    const projData = project.data()
+    console.log(projData.title)
+    blocks2.push({
+      type: `input`,
+      block_id: `projectTitle${index}`,
+      label: {
+        type: `plain_text`,
+        text: `プロジェクト名`,
+        emoji: true,
+      },
+      element: {
+        type: `plain_text_input`,
+        action_id: `projectTitle${index}`,
+        placeholder: {
+          type: `plain_text`,
+          text: `${thisYear}年${thisMonth}月の挑戦`,
+        },
+      },
+    })
+
+    blocks2.push({
+      type: `input`,
+      block_id: `year${index}`,
+      label: {
+        type: `plain_text`,
+        text: `年`,
+        emoji: true,
+      },
+      element: {
+        type: `static_select`,
+        action_id: `year${index}`,
+        placeholder: {
+          type: `plain_text`,
+          text: `年を選択`,
+        },
+        options: [
+          {
+            text: {
+              type: `plain_text`,
+              text: `${thisYear}`,
+              emoji: true,
+            },
+            value: `${thisYear}`,
+          },
+          {
+            text: {
+              type: `plain_text`,
+              text: `${thisYear + 1}`,
+              emoji: true,
+            },
+            value: `${thisYear + 1}`,
+          },
+        ],
+        initial_option: {
+          text: {
+            type: `plain_text`,
+            text: `${thisYear}`,
+            emoji: true,
+          },
+          value: `${thisYear}`,
+        },
+      },
+    })
+    blocks2.push({
+      type: `input`,
+      block_id: `month${index}`,
+      label: {
+        type: `plain_text`,
+        text: `月`,
+        emoji: true,
+      },
+      element: {
+        type: `static_select`,
+        action_id: `month${index}`,
+        placeholder: {
+          type: `plain_text`,
+          text: `月を選択`,
+          emoji: true,
+        },
+        options: monthOptions,
+        initial_option: {
+          text: {
+            type: `plain_text`,
+            text: `${thisMonth}`,
+            emoji: true,
+          },
+          value: `${thisMonth}`,
+        },
+      },
+    })
+
+    blocks2.push({
+      type: `input`,
+      block_id: `challenges${index}`,
+      label: {
+        type: `plain_text`,
+        text: `挑戦`,
+        emoji: true,
+      },
+      hint: {
+        type: `plain_text`,
+        text: `挑戦が複数ある場合は改行します。1行1挑戦として入力できます`,
+      },
+      element: {
+        type: `plain_text_input`,
+        multiline: true,
+        action_id: `challenges${index}`,
+        placeholder: {
+          type: `plain_text`,
+          emoji: true,
+          text: `毎週ブログを書く\n積ん読を1冊解消する`,
+        },
+      },
+    })
+
+    blocks2.push({
+      type: `input`,
+      block_id: `description${index}`,
+      label: {
+        type: `plain_text`,
+        text: `説明`,
+        emoji: true,
+      },
+      optional: true,
+      hint: {
+        type: `plain_text`,
+        text: `プロジェクトに補足説明があれば入力します`,
+      },
+      element: {
+        type: `plain_text_input`,
+        multiline: true,
+        action_id: `description${index}`,
+        placeholder: {
+          type: `plain_text`,
+          text: `説明の内容`,
+        },
+      },
+    })
+  })
+
+  // const blocks: ModalBlock[] = [, , , ,]
+
   try {
     const modal: Modal = {
       token: context.botToken,
@@ -61,141 +207,7 @@ app.command(`/cem_edit`, async ({ payload, ack, context }) => {
           text: `破棄`,
           emoji: true,
         },
-        blocks: [
-          {
-            type: `input`,
-            block_id: `projectTitle`,
-            label: {
-              type: `plain_text`,
-              text: `プロジェクト名`,
-              emoji: true,
-            },
-            element: {
-              type: `plain_text_input`,
-              action_id: `projectTitle`,
-              placeholder: {
-                type: `plain_text`,
-                text: `${thisYear}年${thisMonth}月の挑戦`,
-              },
-            },
-          },
-          {
-            type: `input`,
-            block_id: `year`,
-            label: {
-              type: `plain_text`,
-              text: `年`,
-              emoji: true,
-            },
-            element: {
-              type: `static_select`,
-              action_id: `year`,
-              placeholder: {
-                type: `plain_text`,
-                text: `年を選択`,
-              },
-              options: [
-                {
-                  text: {
-                    type: `plain_text`,
-                    text: `${thisYear}`,
-                    emoji: true,
-                  },
-                  value: `${thisYear}`,
-                },
-                {
-                  text: {
-                    type: `plain_text`,
-                    text: `${thisYear + 1}`,
-                    emoji: true,
-                  },
-                  value: `${thisYear + 1}`,
-                },
-              ],
-              initial_option: {
-                text: {
-                  type: `plain_text`,
-                  text: `${thisYear}`,
-                  emoji: true,
-                },
-                value: `${thisYear}`,
-              },
-            },
-          },
-          {
-            type: `input`,
-            block_id: `month`,
-            label: {
-              type: `plain_text`,
-              text: `月`,
-              emoji: true,
-            },
-            element: {
-              type: `static_select`,
-              action_id: `month`,
-              placeholder: {
-                type: `plain_text`,
-                text: `月を選択`,
-                emoji: true,
-              },
-              options: monthOptions,
-              initial_option: {
-                text: {
-                  type: `plain_text`,
-                  text: `${thisMonth}`,
-                  emoji: true,
-                },
-                value: `${thisMonth}`,
-              },
-            },
-          },
-          {
-            type: `input`,
-            block_id: `challenges`,
-            label: {
-              type: `plain_text`,
-              text: `挑戦`,
-              emoji: true,
-            },
-            hint: {
-              type: `plain_text`,
-              text: `挑戦が複数ある場合は改行します。1行1挑戦として入力できます`,
-            },
-            element: {
-              type: `plain_text_input`,
-              multiline: true,
-              action_id: `challenges`,
-              placeholder: {
-                type: `plain_text`,
-                emoji: true,
-                text: `毎週ブログを書く\n積ん読を1冊解消する`,
-              },
-            },
-          },
-          {
-            type: `input`,
-            block_id: `description`,
-            label: {
-              type: `plain_text`,
-              text: `説明`,
-              emoji: true,
-            },
-            optional: true,
-            hint: {
-              type: `plain_text`,
-              text: `プロジェクトに補足説明があれば入力します`,
-            },
-            element: {
-              type: `plain_text_input`,
-              multiline: true,
-              action_id: `description`,
-              placeholder: {
-                type: `plain_text`,
-                text: `説明の内容`,
-              },
-            },
-          },
-        ],
+        blocks: blocks2,
       },
     }
     return app.client.views.open(modal as any)
