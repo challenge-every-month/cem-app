d2dc453137c59e5aa84bb5de0a975d7dac2b0af4
nainaistar <nainaistar@gmail.com>
2020-04-01 00:57:29 +0900

    add: データを修正してDBに登録

--- src/listeners/res_cem_edit.ts
+++ src/listeners/res_cem_edit.ts
@@ -2,6 +2,26 @@ import { app } from '../initializers/bolt'
 import { FieldValue, firestore } from '../initializers/firebase'
 import { Challenge, Message, Project } from '../types/slack'
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+class ModalDto {
+  // eslint-disable-next-line no-useless-constructor
+  constructor(
+    public projectTitle: string,
+    public year: number,
+    public month: number,
+    public challengeArea: string,
+    public description: string
+  ) {}
+
+  public static toProjectTitle(obj: Object | unknown): string {
+    if (obj instanceof Object) {
+      const a = Object.values(obj)
+      return a[0].value
+    }
+    return ``
+  }
+}
+
 class Key {
   // eslint-disable-next-line no-useless-constructor
   constructor(public key: string) {}
@@ -27,72 +47,7 @@ class Key {
   }
 }
 
-app.view(`cem_edit`, async ({ ack, body, view, context }) => {
-  ack()
-
-  const payload = (view.state as any).values
-
-  // key情報を無理やり取得する
-  for (const [keyTmp, value] of Object.entries(payload)) {
-    const key = new Key(keyTmp)
-    console.log(`${key}`)
-    if (key.isProjectTitle()) {
-      if (value instanceof Object) {
-        const a = Object.values(value)
-        const b = a[0].value
-        console.log(b)
-      }
-    }
-
-    if (key.isYear()) {
-      if (value instanceof Object) {
-        const a = Object.values(value)
-        const b = a[0].selected_option.value
-        console.log(b)
-      }
-    }
-
-    if (key.isMonth()) {
-      if (value instanceof Object) {
-        const a = Object.values(value)
-        const b = a[0].selected_option.value
-        console.log(b)
-      }
-    }
-
-    if (key.isDescription()) {
-      if (value instanceof Object) {
-        const a = Object.values(value)
-        const b = a[0].value
-        // TODO: undefinedだったらブランク
-        console.log(b)
-      }
-    }
-
-    if (key.isChallenge()) {
-      console.log(`ちゃれんじ`)
-      if (value instanceof Object) {
-        const a = Object.values(value)
-        const b = a[0].value
-        console.log(b)
-      }
-    }
-  }
-
-  const user = body.user.id
-  const projectTitle = payload.projectTitle0.projectTitle0.value
-  const year = payload.year0.year0.selected_option.value
-  const month = payload.month0.month0.selected_option.value
-  const description: string = payload.description0
-    ? payload.description0.description0.value || ``
-    : ``
-
-  const challengerRef = firestore.collection(`challengers`).doc(user)
-  const projectsRef = firestore.collection(`projects`)
-  const timestamp = await FieldValue.serverTimestamp()
-
-  // firestoreからプロジェクトとサブコレクションのチャレンジを削除
-  const batch = firestore.batch()
+async function addDeleteBatch(challengerRef, batch) {
   // HACK:呼び出し元で二重発行している
   const projectDeleteQuery = firestore
     .collection(`projects`)
@@ -111,46 +66,125 @@ app.view(`cem_edit`, async ({ ack, body, view, context }) => {
     })
     batch.delete(projectDel)
   }
+}
+
+app.view(`cem_edit`, async ({ ack, body, view, context }) => {
+  ack()
+
+  const user = body.user.id
+  const challengerRef = firestore.collection(`challengers`).doc(user)
+  const projectsRef = firestore.collection(`projects`)
+  const timestamp = await FieldValue.serverTimestamp()
+  // firestoreからプロジェクトとサブコレクションのチャレンジを削除
+  const batch = firestore.batch()
+  await addDeleteBatch(challengerRef, batch)
+
+  const payload = (view.state as any).values
+
+  const modalDtoList: ModalDto[] = []
+
+  let year = 0
+  let month = 0
+  let projectTitle = ``
+  let description = ``
+  let challengeList = ``
+  console.log(payload)
+  // key情報を無理やり取得する
+  for (const [keys, value] of Object.entries(payload)) {
+    const key = new Key(keys)
+    if (key.isProjectTitle()) {
+      projectTitle = ModalDto.toProjectTitle(value)
+    }
+
+    if (key.isYear()) {
+      if (value instanceof Object) {
+        const a = Object.values(value)
+        year = Number(a[0].selected_option.value)
+      }
+    }
+
+    if (key.isMonth()) {
+      if (value instanceof Object) {
+        const a = Object.values(value)
+        month = Number(a[0].selected_option.value)
+      }
+    }
+
+    if (key.isDescription()) {
+      if (value instanceof Object) {
+        const a = Object.values(value)
+        description = a[0].value
+        // TODO: undefinedだったらブランク
+      }
+    }
+
+    if (key.isChallenge()) {
+      if (value instanceof Object) {
+        const a = Object.values(value)
+        challengeList = a[0].value
+      }
+    }
+
+    if (
+      projectTitle !== `` &&
+      year !== 0 &&
+      month !== 0 &&
+      description !== `` &&
+      challengeList !== ``
+    ) {
+      modalDtoList.push(
+        new ModalDto(projectTitle, year, month, challengeList, description)
+      )
+
+      // reset
+      projectTitle = ``
+      year = 0
+      month = 0
+      description = ``
+      challengeList = ``
+    }
+  }
 
   // firestoreにプロジェクト作成
 
-  const project: Project = {
-    challenger: challengerRef,
-    year: Number(year),
-    month: Number(month),
-    title: projectTitle,
-    status: `draft`,
-    description: description,
-    updatedAt: timestamp,
-    createdAt: timestamp,
-  }
-  const projectRef = await projectsRef.add(project).catch(err => {
-    throw new Error(err)
-  })
-
-  // firestoreに挑戦を行ごとにパーズして保存
-  for (const challengeName of payload.challenges0.challenges0.value.split(
-    /\n/
-  )) {
-    if (challengeName === ``) {
-      continue
-    }
-    const challenge: Challenge = {
+  for (const modalDto of modalDtoList) {
+    const project: Project = {
       challenger: challengerRef,
-      year: Number(payload.year0.year0.selected_option.value),
-      month: Number(month),
-      name: challengeName,
+      year: modalDto.year,
+      month: modalDto.month,
+      title: modalDto.projectTitle,
       status: `draft`,
+      description: modalDto.description,
       updatedAt: timestamp,
       createdAt: timestamp,
     }
-    batch.set(projectRef.collection(`challenges`).doc(), challenge)
+    const projectRef = await projectsRef.add(project).catch(err => {
+      throw new Error(err)
+    })
+
+    // firestoreに挑戦を行ごとにパーズして保存
+    for (const challengeName of modalDto.challengeArea.split(/\n/)) {
+      if (challengeName === ``) {
+        continue
+      }
+      const challenge: Challenge = {
+        challenger: challengerRef,
+        year: modalDto.year,
+        month: modalDto.month,
+        name: challengeName,
+        status: `draft`,
+        updatedAt: timestamp,
+        createdAt: timestamp,
+      }
+      batch.set(projectRef.collection(`challenges`).doc(), challenge)
+    }
   }
+
   await batch.commit()
   // 成功をSlack通知
   const msg: Message = {
     token: context.botToken,
-    text: `新規プロジェクト[${projectTitle}]を[${year}-${month}]に登録しました`,
+    text: `プロジェクトを修正しました`,
     channel: body.view.private_metadata,
     user: user,
   }
